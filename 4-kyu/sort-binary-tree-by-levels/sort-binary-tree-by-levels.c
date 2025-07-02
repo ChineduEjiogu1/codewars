b79ea934b1cc5462176d1a107bf1b2e26f4c5202
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2025-07-01 20:58:00 -0400

    Update sort-binary-tree-by-levels.c

--- 4-kyu/sort-binary-tree-by-levels/sort-binary-tree-by-levels.c
+++ 4-kyu/sort-binary-tree-by-levels/sort-binary-tree-by-levels.c
@@ -1,3 +1,48 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+/* to help you solve the kata, a queue implementation has been
+preloaded for you */
+
+typedef struct Queue Queue;
+
+// the queue elements are pointers
+
+// creates a new queue
+extern Queue *new_queue (void);
+// returns the number of elements in the queue
+extern size_t queue_size (const Queue *queue);
+// adds an element at the back of the queue and returns the queue
+extern Queue *queue_enqueue (Queue *queue, const void *data);
+// removes the element at the front of the queue and returns it
+extern void *queue_dequeue (Queue *queue);
+// frees the queue, do not forget to call it !
+extern void free_queue (Queue *queue);
+/* ==================================================== */
+
+typedef struct Tree {
+	struct Tree *left, *right;
+	int value;
+} Tree;
+
+int count_of_tree(const Tree *tree)
+{
+    if (tree == NULL)
+    {
+        return 0;
+    }
+  
+    return count_of_tree(tree->left) + count_of_tree(tree->right) + 1;
+}
+
+int *tree_by_levels (const Tree *tree, size_t *tree_size)
+{
+    
+  if (tree == NULL)
+  {
+      *tree_size = 0;
+      
+      return NULL; 
   } 
   
   int n = count_of_tree(tree);
