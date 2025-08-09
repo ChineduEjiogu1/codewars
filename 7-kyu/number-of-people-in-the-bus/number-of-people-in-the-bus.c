f09177d0945042042ce2954a5bb0831f8fc8792a
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2025-08-09 17:14:17 -0400

    Add solution - CodeHub

---
+++ 7-kyu/number-of-people-in-the-bus/number-of-people-in-the-bus.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+​
+int bus_terminus (size_t nb_stops, const short bus_stops[nb_stops][2])
+{
+    auto bus = 0; // 🚌 :p
+  
+    for (size_t i = 0; i < nb_stops; i++)
+    { 
+       bus += bus_stops[i][0] - bus_stops[i][1]; 
+    }
+    
+    return bus;
+}
