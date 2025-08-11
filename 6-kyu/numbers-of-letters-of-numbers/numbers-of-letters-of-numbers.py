cb83e8d186432cd5ba04a59322572deaef3ad615
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2025-08-10 20:02:29 -0400

    Update solution - CodeHub

--- 6-kyu/numbers-of-letters-of-numbers/numbers-of-letters-of-numbers.py
+++ 6-kyu/numbers-of-letters-of-numbers/numbers-of-letters-of-numbers.py
@@ -1,5 +1,5 @@
 def numbers_of_letters(n):
-    
+​
     string_numbers = {
      0 :'zero',
      1:'one',
@@ -12,19 +12,18 @@ def numbers_of_letters(n):
      8 : 'eight',
      9: 'nine'
 }
-    
+​
     result_list = []
-    
+​
     word_digits = [string_numbers[int(digit)] for digit in str(n)]
     concat_digits = "".join(word_digits)
-            
+​
     while concat_digits != "four" :
         result_list.append(concat_digits)
         letter_count = len(concat_digits)
         word_digits = [string_numbers[int(digit)] for digit in str(letter_count)]
         concat_digits = "".join(word_digits)
-    
+​
     result_list.append("four")
-                                           
+​
     return result_list
-    
