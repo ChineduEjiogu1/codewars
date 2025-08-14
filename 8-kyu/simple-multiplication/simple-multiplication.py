a0ab12d5278c682497632b765c90435e68f4e779
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2025-08-14 11:28:51 -0400

    Add solution - CodeHub

---
+++ 8-kyu/simple-multiplication/simple-multiplication.py
@@ -0,0 +1,10 @@
+def simple_multiplication(number) :
+    # Your code goes here
+    
+    eight = 8
+    nine = 9
+    
+    if number % 2 == 0:
+        return number * eight
+    else:
+        return number * nine
