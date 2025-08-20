fdb0670defb05a3d7e745d770d517ce5846de6d6
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2025-08-19 21:59:14 -0400

    Add solution - CodeHub

---
+++ 8-kyu/keep-hydrated-1/keep-hydrated-1.c
@@ -0,0 +1,12 @@
+#include <math.h>
+​
+int Liters(double time) 
+{
+    //The fun begins here.
+    double liters_per_hour = 0.5;
+  
+    int result = 0;
+  
+    return result = (int)floor(time * liters_per_hour);
+    
+}
