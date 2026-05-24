1eaccd961ada6412463016177237303aa678b09d
Chinedu E. Ejiogu <chineduejiogu@gmail.com>
2026-05-24 00:29:21 -0400

    Add solution - CodeHub

---
+++ 6-kyu/validate-credit-card-number/validate-credit-card-number.js
@@ -0,0 +1,20 @@
+function validate(n) {
+  
+  let creditCard = String(n).split('');
+  
+  let total = 0;
+  let double = 2;
+  let forcedBase10 = 9;
+  
+  for (let i = 0; i < creditCard.length; i++) {
+    let creditCardDigit = Number(creditCard[i]);
+    if (i % 2 === creditCard.length % 2) {
+      creditCardDigit = creditCardDigit * double;
+      if (creditCardDigit > 9) {
+        creditCardDigit = creditCardDigit - forcedBase10;
+      }
+    }
+    total += creditCardDigit;
+  }
+  return total % 10 === 0;
+}
